1b89b3100280282fc7953550cac2296f191e98c7
asjo <asjo>
2004-10-11 11:36:37 +0000

    Document obvius_connect.

--- perl/WebObvius/Site.pm
+++ perl/WebObvius/Site.pm
@@ -1,8 +1,36 @@
-# $Id$
-
 package WebObvius::Site;
 
-use 5.006;
+########################################################################
+#
+# Site.pm - General site object
+#
+# Copyright (C) 2001-2004 Magenta Aps, Denmark (http://www.magenta-aps.dk/)
+#                         aparte A/S, Denmark (http://www.aparte.dk/),
+#                         FI, Denmark (http://www.fi.dk/)
+#
+# Authors: Jørgen Ulrik B. Krag (jubk@magenta-aps.dk)
+#          Peter Makholm (pma@fi.dk)
+#          René Seindal,
+#          Adam Sjøgren (asjo@magenta-aps.dk),
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+########################################################################
+
+# $Id$
+
 use strict;
 use warnings;
 
@@ -70,6 +98,11 @@ sub report_benchmarks {
 #
 ########################################################################
 
+# obvius_connect - given a request object, strings containing username
+#                  and password, array-refs to doctypes, fieldtypes
+#                  and fieldspecs returns an obvius-object if a
+#                  connection could be made. The object is also put on
+#                  notes, key 'obvius'. Returns undef on error.
 sub obvius_connect {
     my ($this, $req, $user, $passwd, $doctypes, $fieldtypes, $fieldspecs) = @_;
 
@@ -617,36 +650,26 @@ sub expire_public_login_cookie {
 
 1;
 __END__
-# Below is stub documentation for your module. You better edit it!
 
 =head1 NAME
 
-WebObvius::Site - Perl extension for blah blah blah
+WebObvius::Site - General site object.
 
 =head1 SYNOPSIS
 
   use WebObvius::Site;
-  blah blah blah
 
 =head1 DESCRIPTION
 
-Stub documentation for WebObvius::Site, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
-=head2 EXPORT
-
-None by default.
-
-
 =head1 AUTHOR
 
-A. U. Thor, E<lt>a.u.thor@a.galaxy.far.far.awayE<gt>
+Jørgen Ulrik B. Krag E<lt>jubk@magenta-aps.dkE<gt>
+Peter Makholm E<lt>pma@fi.dk<gt>
+René Seindal
+Adam Sjøgren E<lt>asjo@magenta-aps.dk<gt>
 
 =head1 SEE ALSO
 
-L<perl>.
+L<WebObvius>.
 
 =cut
