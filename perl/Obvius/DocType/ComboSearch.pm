93babf17fed54409c6976f89009741f67f74183f
asjo <asjo>
2004-11-26 15:20:46 +0000

    Fix up the long standing bug that meant that 'not'-searches on repeatable fields gave too many results back.
    It's done by pruning the result according to the parse-tree of the
    search-expression *if necessary*.
    
    Measuring the time spent filtering on all the ComboSearch documents on
    DEG and EUO gave around 1-2s extra in worst case, usually below 0.1s
    (because filtering is only needed quite rarely).
    
    Example-search here:
    http://deg.rovereto.magenta-aps.dk/admin/data/emner/emne004/potteplanter/
    (used to return 130+, now returns around 30, running time is 1.2s
    longer on my desktop).

--- perl/Obvius/DocType/ComboSearch.pm
+++ perl/Obvius/DocType/ComboSearch.pm
@@ -133,7 +133,7 @@ sub action {
 
     # doctype is actually type:
     #print STDERR "WHERE1: $where\n";
-    $where =~ s/([^\w])doctype(\s?)([^\w]+)(\s?)['\"]([^'\"]+)['\"]/$this->doctypemap($1, $2, $3, $4, $5, $obvius)/egi;
+    $where =~ s/([^\w])doctype(\s?)([^\w]+)(\s?)[\'\"]([^\'\"]+)[\'\"]/$this->doctypemap($1, $2, $3, $4, $5, $obvius)/egi;
     $obvius->log->debug("ComboSearch: WHERE2: $where");
 
     # Doublecheck search_fields. Make sure we actually have a vfield for each.
@@ -146,18 +146,66 @@ sub action {
 
     $output->param(Obvius_DEPENCIES => 1);
     # nothidden, notexpired, public
+
     my $vdocs = $obvius->search(\@search_fields, $where,
 			      sortvdoc=>$vdoc,
 			      notexpired=>!$is_admin,
 			      public=>!$is_admin,
 			      override_repeatable => \%no_repeatable,
-			      %extra_search_options
+			      %extra_search_options,
 			     );
+
+    unless ($vdocs) {
+        return OBVIUS_OK;
+    }
 
     #print STDERR "vdoc: " . Dumper($vdoc);
     #print STDERR "search-result: " . Dumper($vdocs);
-    unless ($vdocs) {
-        return OBVIUS_OK;
+
+    # If the search was on repeatable fields and the conditions
+    # thereupon were negated (NOT IN, !=, NOT LIKE, other?), Obvius::search
+    # returns to much.
+    #
+    # For example: the search X NOT IN (A, B) on:
+    #
+    #  docid version field value
+    #    N      M      X     A
+    #    N      M      X     B
+    #    N      M      X     C
+    #
+    # includes (N, M) in the resultset, which is natural given how the
+    # database works, but it's not what is expected.
+    #
+    # How can we fix this? Well, since Obvius::search() gives us all
+    # the correct results, plus some extra, we can get the right
+    # results by filtering away the false positives.
+    #
+    # (It could be argued that this should be fixed generally in
+    # Obvius::search(), and I tend to agree, but I'd rather start it
+    # off here, as we have a nice parsetree to work from, making it
+    # much easier to filter here than to parse the where-expression
+    # that Obvius::search takes and parsing that to do extra
+    # filtering).
+    #
+    # We only do the filtering if we are searching on at least one
+    # repeatable field:
+    my $repeatable=scalar(grep {
+        my $fspec=$obvius->get_fieldspec($_);
+        $fspec ? $fspec->Repeatable : 0 } @search_fields);
+
+    if ($repeatable) {
+        my $parser=Obvius::DocType::ComboSearch::Parser->new();
+        my $tree = $parser->parse($program);
+        my $filtered_vdocs=filter_result($vdocs, $tree, $obvius);
+
+        # We need to keep the ordering, so we use the filtered_docs as
+        # a filter (slight sigh):
+        if (scalar(@$filtered_vdocs)!=scalar(@$vdocs)) {
+            my %still_there=map { $_->Docid . ':' . $_->Version => 1 } @$filtered_vdocs;
+            my @new_vdocs=(grep { $still_there{$_->Docid . ':' . $_->Version} } @$vdocs);
+
+            $vdocs=\@new_vdocs;
+        }
     }
 
     # This is handled by search when public is 1!
@@ -196,6 +244,118 @@ sub action {
     return OBVIUS_OK;
 }
 
+# filter_result - given an array-ref to version-objects and an
+#                 array-ref to a combosearch parse-tree, performs an
+#                 extra pruning of the version-objects handling some
+#                 of the the parse-tree restrictions that
+#                 Obvius::search can't ("negative" operations on
+#                 repeatable fields).
+sub filter_result {
+    my ($vdocs, $tree, $obvius)=@_;
+
+    my @all_vdocs=@$vdocs;
+
+    return do_filter_result(\@all_vdocs, $tree, $obvius);
+}
+
+sub do_filter_result {
+    my ($result, $tree, $obvius)=@_;
+
+    my $op=shift @$tree;
+    if ($op eq 'OR') {
+        return or_result($result, $tree, $obvius);
+    }
+    elsif ($op eq 'AND') {
+        return and_result($result, $tree, $obvius);
+    }
+    else {
+        my ($field, $arg)=@$tree;
+
+        my $fieldspec=$obvius->get_fieldspec($field);
+        if (!defined $fieldspec) {
+            # No fieldspec, assuming non-repeatable; don't care:
+            return $result
+        }
+        if (defined $fieldspec and !$fieldspec->Repeatable) {
+            # Not repeatable, we don't care about it:
+            return $result;
+        }
+
+        # HANDLE some ops manually when the field is repeatable:
+        #  NOT IN and != are handled, the others are ignored:
+        #  (> and < do not make a whole lot of sense for repeatables...)
+        if ($op eq 'LIKE' or $op eq 'IN' or $op eq '=' or $op eq '>' or $op eq '<') {
+            # op is a boring op, we don't care:
+            return $result;
+        }
+        elsif ($op eq 'NOT IN') {
+            return not_in($result, $field, $arg, $obvius);
+        }
+        elsif ($op eq '!=') {
+            return not_in($result, $field, $arg, $obvius);
+        }
+        # XXX TODO: handle 'NOT LIKE'
+        else {
+            print STDERR "ComboSearch::do_filter_result: unknown op $op; fix me!\n";
+            return $result;
+        }
+    }
+}
+
+sub not_in_test {
+    my ($arg, $value)=@_;
+
+    my %not=map { $_=>1 } split /\|/, $arg;
+    my $res=grep { $not{$_->Id} } @$value;
+
+    return ($res ? 0 : 1);
+}
+
+sub not_in {
+    my ($vdocs, $field, $arg, $obvius)=@_;
+
+    my @new_result=();
+    foreach my $vdoc (@$vdocs) {
+        my $value=$obvius->get_version_field($vdoc, $field);
+        push @new_result, $vdoc if (not_in_test($arg, $value));
+    }
+
+    return \@new_result;
+}
+
+sub or_result {
+    my ($result, $tree, $obvius)=@_;
+
+    my %new_result=();
+    foreach my $node (@$tree) {
+        my $node_result=do_filter_result($result, $node, $obvius);
+        foreach my $vdoc (@$node_result) {
+            $new_result{$vdoc->Docid . ':' . $vdoc->Version}=$vdoc;
+        }
+    }
+
+    return [ map { $new_result{key} } keys %new_result ];
+}
+
+sub and_result {
+    my ($result, $tree, $obvius)=@_;
+
+    my %objects=();
+    my %count=();
+    my $numnodes=0;
+    foreach my $node (@$tree) {
+        $numnodes++;
+        my $node_result=do_filter_result($result, $node, $obvius);
+        foreach my $vdoc (@$node_result) {
+            # Check node_result-type!
+            # http://euo.rovereto.magenta-aps.dk/euo_ensubjects/consumer-health/
+            $objects{$vdoc->Docid . ':' . $vdoc->Version}=$vdoc;
+            $count{$vdoc->Docid . ':' . $vdoc->Version}++;
+        }
+    }
+
+    return [ map { $objects{$_} } grep { $count{$_}==$numnodes } keys %count ];
+}
 
 1;
 __END__
