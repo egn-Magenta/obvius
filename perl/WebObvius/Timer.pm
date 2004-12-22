b5c6875d647d8fff08f13573431467608ce21b86
asjo <asjo>
2004-12-22 11:15:23 +0000

    Document Timer.pm.

--- perl/WebObvius/Timer.pm
+++ perl/WebObvius/Timer.pm
@@ -1,5 +1,31 @@
 package WebObvius::Timer;
 
+########################################################################
+#
+# Timer.pm - perl-module for logging time for each request.
+#
+# Copyright (C) 2002 FI, Denmark (http://www.fi.dk/)
+#
+# Authors: Peter Makholm (pma@fi.dk)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+########################################################################
+
+# $Id$
+
 use strict;
 use warnings;
 use Apache::Constants qw(:common);
@@ -11,12 +37,16 @@ our @EXPORT_OK = qw( handler );
 
 our ( $VERSION ) = '$Revision$ ' =~ /\$Revision:\s+([^\s]+)/;
 
+# StartTimer - stores the current time on pnotes for use by LogTimer
+#              later. To be used as a PerlPostReadRequestHandler.
 sub StartTimer {
     my $r = shift;
     $r->pnotes(entrytime => [gettimeofday()]); 
     return OK;
 }
 
+# LogTimer - logs remote host, date, status, bytes and duration for a
+#            request. To be used as a PerlLogHandler.
 sub LogTimer {
     my $r = shift;
     my $logfile = $r->dir_config('TimerLog') || return OK;
@@ -38,3 +68,25 @@ __END__
 =head1 NAME
 
 WebObvius::Timer - Perl module for logging status, bytes and time of each request.
+
+=head1 SYNOPSIS
+
+    # Logging af timer:
+    PerlModule WebObvius::Timer
+    PerlPostReadRequestHandler WebObvius::Timer::StartTimer
+    PerlLogHandler WebObvius::Timer::LogTimer
+    PerlSetVar TimerLog /var/www/www.example.com/logs/timer_log
+
+=head1 DESCRIPTION
+
+Adding the above lines to the configuration-file of a website
+(www.example.com/conf/setup.conf) will add logging of request-duration
+to www.example.com/conf/timer_log.
+
+=head1 AUTHOR
+
+Peter Makholm, E<lt>pma@fi.dkE<gt>
+
+=head1 SEE ALSO
+
+=cut
