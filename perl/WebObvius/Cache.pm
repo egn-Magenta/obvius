48f751bf3949cee4d64f9c70e4523ac5378a0eeb
asjo <asjo>
2005-01-11 12:57:18 +0000

    Use a temporary variable to store the mimetype.
    Add boilerplate and documentation.

--- perl/WebObvius/Cache.pm
+++ perl/WebObvius/Cache.pm
@@ -1,7 +1,37 @@
 package WebObvius::Cache;
 
+########################################################################
+#
+# Cache.pm - simple perl-module that sets content-type from the path.
+#
+# Copyright (C) 2002-2004 ForbrugerInformationen,
+#                         Magenta Aps, Denmark (http://www.magenta-aps.dk/)
+#                         aparte A/S, Denmark (http://www.aparte.dk/),
+#
+# Authors: Peter Makholm (pma@fi.dk)
+#          Adam Sjøgren (asjo@magenta-aps.dk).
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+########################################################################
+
+# $Id$
+
 use strict;
 use warnings;
+
 use Apache::Constants qw(:common);
 
 require Exporter;
@@ -16,8 +46,9 @@ sub handler {
     my $filename = $r->filename;
 
     $filename =~ s/^$prefix//;
-    $filename =~ m|([^/]*/[^/]*)|; 
-    $r->content_type($1);
+    $filename =~ m|([^/]*/[^/]*)|;
+    my $mimetype=$1; # Using $1 directly below sometimes, only sometimes, fails, it seems.
+    $r->content_type($mimetype);
 
     # Most browsers doesn't understand get filenames ending in a slash.
     # Therefore we should tell them what filename to use.
@@ -33,5 +64,25 @@ __END__
 
 =head1 NAME
 
-WebObvius::Cache - Perl module for setting content-type from cache-dir
-                   path.
+WebObvius::Cache - mod_perl module for fixing the mime-type of cache-files.
+
+=head1 SYNOPSIS
+
+<Directory /var/www/www.website/docs/cache>
+    PerlTypeHandler WebObvius::Cache
+    PerlSetVar CachePrefix /var/www/www.website/docs/cache/
+</Directory>
+
+=head1 DESCRIPTION
+
+Parses the path for the mime-type, sets the content_type on the
+request.
+
+=head1 AUTHORS
+
+Peter Makholm <pma@fi.dk>
+Adam Sjøgren <asjo@magenta-aps.dk>
+
+=head1 SEE ALSO
+
+=cut
