a8c387c7785ee3537860e7cf66ff4ddac801c284
martin <martin>
2005-01-17 15:24:04 +0000

    The code now generates RSS feeds based upon a ComboSearch expression.

--- perl/Obvius/DocType/RSSFeed.pm
+++ perl/Obvius/DocType/RSSFeed.pm
@@ -31,13 +31,81 @@ use strict;
 use warnings;
 
 use Obvius;
+use Obvius::Data;
 use Obvius::DocType;
+use Obvius::DocType::ComboSearch;
+
+use XML::RSS;
 
 our @ISA = qw( Obvius::DocType );
 our ( $VERSION ) = '$Revision$ ' =~ /\$Revision:\s+([^\s]+)/;;
 
+# raw_document_data - Returns raw XML data plus the correct mimetype
+sub raw_document_data {
+    my ($this, $doc, $vdoc, $obvius, $input) = @_;
 
-# Preloaded methods go here.
+	my $rss = new XML::RSS (version => '1.0');
+	my $sitename = "http://" . $obvius->{OBVIUS_CONFIG}->param('sitename');
+
+	$rss->channel(link => $sitename);
+	#Mapping of $vdoc fields to RSS equivalents
+	my %field_map = (
+				   'title' => 'title',
+				   'teaser' => 'description'
+				   );
+
+	$obvius->get_version_fields($vdoc, [keys(%field_map)]);
+	foreach my $field (keys(%field_map)) {
+		$rss->channel($field_map{$field} => $vdoc->field($field));
+	}
+
+	#Put Dublin core information on the feed itself...
+	my %dc_field_map = (
+						'docdate' => 'date',
+						'teaser' => 'description'
+						);
+
+	$obvius->get_version_fields($vdoc, [keys (%dc_field_map)]);
+	my %feed_dc;
+	foreach my $field (keys(%dc_field_map)) {
+		$feed_dc{ $dc_field_map{$field}} = $vdoc->field($field);
+	}
+	$feed_dc{'lang'} = $vdoc->param('lang') || 'da';
+	$rss->channel(dc => \%feed_dc);
+
+	#Fetch items using the ComboSearch doctype
+	my $doctype = $obvius->get_version_type($vdoc);
+	my $fake_output = new Obvius::Data;
+	my $fake_input = new Obvius::Data;	
+	my $search = $obvius->get_doctype_by_name('ComboSearch');
+
+	$search->action($fake_input,$fake_output,$doc,$vdoc,$obvius);
+
+	#Insert search results into RSS feed
+	foreach my $result_vdoc (@{$fake_output->{SESSION}->{docs}}) {
+		my $result_doc = $obvius->get_doc_by_id($result_vdoc->DocId);
+		my $url = $sitename . $obvius->get_doc_uri($result_doc);
+		$obvius->get_version_fields($result_vdoc, [qw (teaser docdate)]);
+
+		#Construct item DC information
+		$obvius->get_version_fields($result_vdoc, [keys(%dc_field_map)]);
+		my %item_dc;
+		foreach my $field (keys(%dc_field_map)) {
+			$item_dc{ $dc_field_map{$field}} = $result_vdoc->field($field);
+		}
+		$item_dc{'lang'} = $result_vdoc->param('lang') || 'da';
+		
+		$rss->add_item(
+					   title => $result_vdoc->field('title'),
+					   link => $url,
+					   description => $result_vdoc->field('teaser'),
+					   dc => \%item_dc
+					   );
+	}
+	
+	return ('text/xml', $rss->as_string);
+}
+
 
 1;
 __END__
