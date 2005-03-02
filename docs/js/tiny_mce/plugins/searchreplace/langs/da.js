7399f8b5fccb770da2dc4d063d6da00ab9920014
jubk <jubk>
2005-03-02 14:18:58 +0000

    Danish translations for the search/replace plugin.

---
+++ docs/js/tiny_mce/plugins/searchreplace/langs/da.js
@@ -0,0 +1,19 @@
+// DA lang variables
+
+tinyMCELang['lang_searchreplace_search_desc'] = 'S&#248;g';
+tinyMCELang['lang_searchreplace_searchnext_desc'] = 'S&#248;g igen';
+tinyMCELang['lang_searchreplace_replace_desc'] = 'S&#248;g/Erstat';
+tinyMCELang['lang_searchreplace_notfound'] = 'S&#248;gningen er f&#230;rdig. S&#248;gestrengen blev ikke fundet.';
+tinyMCELang['lang_searchreplace_search_title'] = 'S&#248;g';
+tinyMCELang['lang_searchreplace_replace_title'] = 'S&#248;g/Erstat';
+tinyMCELang['lang_searchreplace_allreplaced'] = 'Alle tilst&#230;dev&#230;relser af s&#248;gestrengen er blevet erstattet.';
+tinyMCELang['lang_searchreplace_findwhat'] = 'Find hvad';
+tinyMCELang['lang_searchreplace_replacewith'] = 'Erstat med';
+tinyMCELang['lang_searchreplace_direction'] = 'Retning';
+tinyMCELang['lang_searchreplace_up'] = 'Op';
+tinyMCELang['lang_searchreplace_down'] = 'Ned';
+tinyMCELang['lang_searchreplace_case'] = 'Match store/sm&#229; bogstaver';
+tinyMCELang['lang_searchreplace_findnext'] = 'Find&nbsp;n&#230;ste';
+tinyMCELang['lang_searchreplace_replace'] = 'Erstat';
+tinyMCELang['lang_searchreplace_replaceall'] = 'Erstat&nbsp;alle';
+tinyMCELang['lang_searchreplace_cancel'] = 'Annuller';
