3317ebe34a051d262494d45eb9bd642bfc767a5b
jubk <jubk>
2005-03-09 12:35:52 +0000

    UTF-8 friendly handling of danish characters.

--- docs/js/tiny_mce/plugins/searchreplace/langs/da.js
+++ docs/js/tiny_mce/plugins/searchreplace/langs/da.js
@@ -1,19 +1,23 @@
 // DA lang variables
 
-tinyMCELang['lang_searchreplace_search_desc'] = 'S&#248;g';
-tinyMCELang['lang_searchreplace_searchnext_desc'] = 'S&#248;g igen';
-tinyMCELang['lang_searchreplace_replace_desc'] = 'S&#248;g/Erstat';
-tinyMCELang['lang_searchreplace_notfound'] = 'S&#248;gningen er f&#230;rdig. S&#248;gestrengen blev ikke fundet.';
-tinyMCELang['lang_searchreplace_search_title'] = 'S&#248;g';
-tinyMCELang['lang_searchreplace_replace_title'] = 'S&#248;g/Erstat';
-tinyMCELang['lang_searchreplace_allreplaced'] = 'Alle tilst&#230;dev&#230;relser af s&#248;gestrengen er blevet erstattet.';
+var ae = unescape('%E6');
+var oe = unescape('%F8');
+var aa = unescape('%E5');
+
+tinyMCELang['lang_searchreplace_search_desc'] = 'S' + oe + 'g';
+tinyMCELang['lang_searchreplace_searchnext_desc'] = 'S' + oe + 'g igen';
+tinyMCELang['lang_searchreplace_replace_desc'] = 'S' + oe + 'g/Erstat';
+tinyMCELang['lang_searchreplace_notfound'] = 'S' + oe + 'gningen er f' + ae + 'rdig. S' + oe + 'gestrengen blev ikke fundet.';
+tinyMCELang['lang_searchreplace_search_title'] = 'S' + oe + 'g';
+tinyMCELang['lang_searchreplace_replace_title'] = 'S' + oe + 'g/Erstat';
+tinyMCELang['lang_searchreplace_allreplaced'] = 'Alle tilst' + ae + 'dev' + ae + 'relser af s' + oe + 'gestrengen er blevet erstattet.';
 tinyMCELang['lang_searchreplace_findwhat'] = 'Find hvad';
 tinyMCELang['lang_searchreplace_replacewith'] = 'Erstat med';
 tinyMCELang['lang_searchreplace_direction'] = 'Retning';
 tinyMCELang['lang_searchreplace_up'] = 'Op';
 tinyMCELang['lang_searchreplace_down'] = 'Ned';
-tinyMCELang['lang_searchreplace_case'] = 'Match store/sm&#229; bogstaver';
-tinyMCELang['lang_searchreplace_findnext'] = 'Find&nbsp;n&#230;ste';
+tinyMCELang['lang_searchreplace_case'] = 'Match store/sm' + aa + ' bogstaver';
+tinyMCELang['lang_searchreplace_findnext'] = 'Find&nbsp;n' + ae + 'ste';
 tinyMCELang['lang_searchreplace_replace'] = 'Erstat';
 tinyMCELang['lang_searchreplace_replaceall'] = 'Erstat&nbsp;alle';
 tinyMCELang['lang_searchreplace_cancel'] = 'Annuller';
