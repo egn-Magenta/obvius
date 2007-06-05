f7d0628af0ed0728ca7802be723ad03c3c054a81
ole <ole>
2007-06-05 17:05:30 +0000

    - Introducing a new document type: ErrorDocument.    ErrorDocument.pm looks for the original request uri and puts it in the    output object. Then the mason template displays the error, and it introduces    a new content variable. If the user enters $url in the content field, it will    automaticly be translated to the original URL, which could not be found.
       The ErrorDocument doctype can be setup in setup.conf like this:
       <Location /public>
         ErrorDocument 404 /error_documents/404/
       </Location>

---
+++ perl/Obvius/DocType/ErrorDocument.pm
@@ -0,0 +1,59 @@
+package Obvius::DocType::ErrorDocument;
+
+# $Id$
+
+use strict;
+use warnings;
+
+use Obvius;
+use Obvius::DocType;
+
+our @ISA = qw( Obvius::DocType );
+our ( $VERSION ) = '$Revision$ ' =~ /\$Revision:\s+([^\s]+)/;
+
+sub action {
+    my ($this, $input, $output, $doc, $vdoc, $obvius) = @_;
+
+    my $org_uri = $input->THE_REQUEST || "";
+    $org_uri =~ s/GET\s//;
+    $org_uri =~ s/HTTP\/\d.\d//;
+    $output->param('ORG_URI' => $org_uri );
+
+    return OBVIUS_OK;
+}
+
+1;
+__END__
+# Below is stub documentation for your module. You better edit it!
+
+=head1 NAME
+
+Obvius::DocType::ErrorDocument - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use Obvius::DocType::ErrorDocument;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for Obvius::DocType::ErrorDocument, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head2 EXPORT
+
+None by default.
+
+
+=head1 AUTHOR
+
+A. U. Thor, E<lt>a.u.thor@a.galaxy.far.far.awayE<gt>
+
+=head1 SEE ALSO
+
+L<perl>.
+
+=cut
