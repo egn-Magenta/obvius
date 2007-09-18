e376433dbccaaa1074a36f26e4a3c354cec58c2a
ole <ole>
2007-09-18 10:55:57 +0000

    - Added perl module for Tooltip

---
+++ perl/Obvius/DocType/Tooltip.pm
@@ -0,0 +1,102 @@
+package Obvius::DocType::Tooltip;
+
+########################################################################
+#
+# Link.pm - Link Document Type
+#
+# Copyright (C) 2001-2004 aparte A/S, Denmark (http://www.aparte.dk),
+#                         Magenta Aps, Denmark (http://www.magenta-aps.dk/)
+#
+# Author: Adam Sjøgren (asjo@magenta-aps.dk)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. 
+#
+########################################################################
+
+# $Id$
+
+use strict;
+use warnings;
+
+use Obvius;
+use Obvius::DocType;
+
+our @ISA = qw( Obvius::DocType );
+our ( $VERSION ) = '$Revision$ ' =~ /\$Revision:\s+([^\s]+)/;
+
+# action - just returns OK, as the "work" of the document type is done
+#          by the alternate_location-method.
+sub action {
+    my ($this, $input, $output, $doc, $vdoc, $obvius) = @_;
+
+    return OBVIUS_OK;
+}
+
+sub raw_document_data {
+    my ( $this, $doc, $vdoc, $obvius, $input) = @_; 
+    return unless $input->param('obvius_bare');
+
+    my $text;
+    $text = "<img src='/grafik/close.gif' class='close' onclick='ajax_hideTooltip();' />";
+
+    # If text is enot set, use the parent-text.
+    $obvius->get_version_field($vdoc, 'content');
+    $obvius->get_version_field($vdoc, 'parent');
+
+    if ($vdoc->field('content') =~ /^\s*$/ && !($vdoc->field('parent') eq '')) {
+	$doc = $obvius->lookup_document($vdoc->field('parent'));
+        $vdoc = $obvius->get_public_version($doc);
+        $vdoc ||= $obvius->get_latest_version($doc);
+        $obvius->get_version_field($vdoc, 'content');
+
+        if(defined $doc && defined $vdoc) {
+            $text .= $vdoc->field('content');
+        } else {
+            $text .="";
+        }
+    } else {
+        $text .= $vdoc->field('content');
+    }
+
+    return ( 'text/html', $text );
+
+}
+
+
+
+1;
+__END__
+
+=head1 NAME
+
+Obvius::DocType::Tooltip - implements the Tooltip document type for Obvius.
+
+=head1 SYNOPSIS
+
+  use'd automatically by Obvius.
+
+=head1 DESCRIPTION
+
+blah blah blah
+
+=head1 AUTHOR
+
+Ole Hejlskov E<lt>ole@magenta-aps.dkE<gt>
+
+=head1 SEE ALSO
+
+L<Obvius::DocType>.
+
+=cut
