82df14819f4a928ba1cebfaf010329160b30bb91
troels <troels>
2008-02-22 15:21:59 +0000

    CustomDb, now somewhat able to handle general queries.

--- perl/WebObvius/Storage/CustomDb.pm
+++ perl/WebObvius/Storage/CustomDb.pm
@@ -16,38 +16,82 @@ sub new {
     return $this;
 }
 
-sub list {
-     my ($this, $object, $options) = @_;
+sub query_append {
+     my ($this, $o) = @_;
+     
+     my $as = "";
+     my @args;
 
-     my $list_sql = $this->{list_sql};
-     die "No list sql " if (!$list_sql);
+     if ($o->{sort}) {
+	  $as .= "ORDER BY " . $o->{sort} . ($o->{reverse} ? ' DESC ' : ' ASC ');
+     }
+     if ($o->{start}) {
+	  $as .= ' OFFSET ? ';
+	  push @args, $o->{start};
+     }
+     if ($o->{max}) {
+	  $as .= ' LIMIT ? ';
+	  push @args, $o->{max}
+     }
+
+     return ($as, \@args);
+}
+
+sub exec_query {
+     my ($this, $query, $args, $handler, $options) = @_;
+
+     my ($as, $append_args) = $this->query_append($options) if $options;
+     
+     $query .= $as if ($as);
+     my @args = @$args;
+     @args = (@args, @$append_args) if ($append_args);
 
      my $dbh = $this->{obvius}->{DB}->DBHdl;
      die "No dbhdl in obvius" if (!$dbh);
 
-     my $sth = $dbh->prepare($list_sql);
-     $sth->execute;
+     my $sth = $dbh->prepare($query);
+     $sth->execute(@args);
 
      my @res;
-
      while (my $row = $sth->fetchrow_hashref) {
-	  push @res, {
-		     map { $_ =>
-			   {
-			    value => $row ->{$_},
-			    status => 'OK'
-			   }
-		      } keys %$row
-		     };
+	  push @res, &$handler($row);
      }
 
      $sth->finish;
-     return \@res, length (@res);
+     return \@res;
+}
+
+sub list {
+     my ($this, $object, $options) = @_;
+
+     my $ps = $this->{prepare_statement};
+     die "No prepare statement" if (!$ps);
+
+     my @args = map { $object->{$_} } @{$this->{args}} if ($this->{args});
+     for (@args) {
+	  die "unknown arguments in args list" if (!$_);
+     }
+
+     my $res = $this->exec_query($ps, \@args, 
+				 sub { return {
+				      map { $_ =>
+					    {
+					     value => $_[0]->{$_},
+					     status => 'OK'
+					    }
+				       } keys %{$_[0]}
+				  }
+				  }, $options);
+
+     my $total_count = $this->exec_query($this->{count_statement}, \@args, sub { return $_[0]->{count} })->[0];
+     
+     return $res, $total_count;
 }
 
 sub lookup {
-     my @a = @_;
-     die Dumper(\@a);
+     my ($this, @a) = @_;
+
+     return;
 }
 
 1;
