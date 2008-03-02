938b7c4a0ad4ae4d0427a32cb80eaea3118c9d30
troels <troels>
2008-03-02 16:00:29 +0000

    ApacheCache v1.0 for clearing document_cache.txt.

--- perl/WebObvius/Cache/ApacheCache.pm
+++ perl/WebObvius/Cache/ApacheCache.pm
@@ -1,20 +1,24 @@
-package ApacheCache;
+package WebObvius::Cache::ApacheCache;
+
+use strict;
+use warnings;
 
 use Fcntl ':flock';
-use MD5 qw (md5_hex);
+use Digest::MD5 qw (md5_hex);
 
+use Data::Dumper;
 use Obvius::Hostmap;
 
 use Exporter;
 
 our @ISA = qw(Exporter);
 
-@EXPORT_OK = qw(is_relevant_for_related_cache);
+our @EXPORT_OK = qw(is_relevant_for_leftmenu_cache);
 
-our %known_options = {
+our %known_options = (
     cache_index => 1,
     cache_dir => 1
-};
+);
 
 
 sub new {
@@ -24,11 +28,11 @@ sub new {
 	warn "ApacheCache: Unknow option <$_>" if (!$known_options{$_});
     }
 
-    my $new = {obvius => $obvius, options => %options};
+    my $new = {obvius => $obvius, %options};
 
-    my $var_dir = '/var/www/' . $obvius->{CONFIG}->{sitename} . '/var/';
-    $new->{cache_dir} ||= $obvius->{CONFIG}->{CACHE_DIRECTORY} || (var_dir . 'document_cache/');
-    $new->{cache_index} ||= $obvius->{CONFIG}->{CACHE_INDEX}   || (var_dir . 'document_cache.txt');
+    my $var_dir = '/var/www/' . $obvius->{OBVIUS_CONFIG}{SITENAME} . '/var/';
+    $new->{cache_dir} ||= $obvius->{OBVIUS_CONFIG}{CACHE_DIRECTORY} || ($var_dir . 'document_cache/');
+    $new->{cache_index} ||= $obvius->{OBVIUS_CONFIG}{CACHE_INDEX}   || ($var_dir . 'document_cache.txt');
     $new->{cache_dir} .= '/' if ($new->{cache_dir} !~ m|/$|);
 
     die "ApacheCache: " . $new->{cache_dir} . " is not a directory\n" if 
@@ -47,11 +51,11 @@ sub can_request_use_cache_p {
     my ($this, $req) = @_;
 
     my $output = $req->pnotes('OBVIUS_OUTPUT');
-
+    
     return !(
 	($output && $output->param('OBVIUS_SIDE_EFFECTS'))	||
 	$req->no_cache						||
-	$req->method_number != M_GET				||
+	$req->method_number != 0				|| # 0 er M_GET, mod_perl bug.
 	$req->notes('nocache')
 	);
 }
@@ -79,14 +83,14 @@ sub save_request_result_in_cache
     close F;
 
     #Save image info.
-    my ($args) = ($req->args =~ /(?:^|&)(size=\d+(?:x\d+|\%))(?:$|&)/);
+    my ($args) = ($req->args =~ /(?:^|&)(size=\d+(?:x\d+|\%))(?:$|&)/) if ($req->args);
     $args ||= "";
 
     my $path=$req->uri();
 
     open F, ">>", $this->{cache_index} || (warn "Failed to open " . $this->{cache_index}, return);
     flock F, LOCK_EX || (warn "couldn't get lock", goto close);
-    print $fh $path,$args, "\t", '/cache/' . $fn, "\n";
+    print F $path, $args, "\t", '/cache/' . $fn, "\n";
     flock F, LOCK_UN;
 
   close:
@@ -99,39 +103,40 @@ sub flush {
     my ($this, $commands) = @_;
 
     $commands = [$commands] if (ref($commands) ne 'ARRAY');
-
-    my %flush_simple = map { $_->{url} => 1} 
-        grep {$_->{command} eq 'clear_url' } @$commands;
-    my @flush_regexps = map { qr/$_->{regexp}/ } 
+    
+    my %flush_simple = map { lc $_->{uri} => 1} 
+        grep {$_->{command} eq 'clear_uri' } @$commands;
+    my @flush_regexps = map { qr/$_->{regexp}/i } 
 	grep {$_->{command} eq 'clear_by_regexp'} @$commands;
-    my @flush_not_regexps = map { qr/$_->{regexp}/ } 
-	grep {$_->{command} eq 'clear_by_note_regexp'} @$uri;
-      
+    my @flush_not_regexps = map { qr/$_->{regexp}/i } 
+	grep {$_->{command} eq 'clear_by_not_regexp'} @$commands;
     
     return $this->flush_by_pattern(
             sub {
-		 $flushes{$_[0]} and return 1;
-		 $_[0] =~ /$_/ and return 1 for (@flush_regexps);
-		 $_[0] !~ /$_/ and return 1 for (@flush_not_regexps);
-
+		 my $uri = shift;
+		 $flush_simple{lc $uri} and return 1;
+		 $uri =~ /$_/ and return 1 for (@flush_regexps);
+		 $uri !~ /$_/ and return 1 for (@flush_not_regexps);
+		 
 		 return 0;
 	    });
 }
 
 sub flush_by_pattern {
-    my ($this, $keep) = @_;
+    my ($this, $pred) = @_;
 
-    open F, '<+', $this->{cache_index} || return;
+    open F, '+<', $this->{cache_index} || return;
     flock F, LOCK_EX || goto close;
     my @lines;
-    while(<F>) {
-        ($local_uri) = m/^(\S+)/;
-	push @lines, $_ if (! &$pred($_));
+    while(my $line = <F>) {
+        my ($local_uri) = ($line =~ m/^(\S+)/);
+	push @lines, $line if ($local_uri && !(&$pred($local_uri)));
     }
-    truncate F, 0
-    print F @lines;
+    seek F, 0, 0;
+    truncate F, 0;
+    print F $_ for (@lines);
     
-    flock F, LOCK_UN
+    flock F, LOCK_UN;
   close:
     close F;
     
@@ -141,7 +146,10 @@ sub flush_by_pattern {
 sub execute_query {
      my ($this, $sql, @args) = @_;
 
-     my $sth = $this->{obvius}->{DB}->DBHdl->prepare($sql);
+     my $obvius = $this->{obvius};
+     $obvius->connect if (!$obvius->{DB});
+     my $sth = $obvius->{DB}->DBHdl->prepare($sql);
+     
      $sth->execute(@args);
      my @res;
 
@@ -149,15 +157,16 @@ sub execute_query {
 	  push @res, $row;
      }
      
+     $sth->finish;
      return \@res;
 }
 
 sub check_vfields_for_docids {
-     my ($this, @docids) = @_;
+     my ($this, $docids) = @_;
      
-     @docids = grep { /^\d+$/ } @docids;
+     my @docids = grep { /^\d+$/ } @$docids;
      return if (!scalar @docids);
-     my @regexp_query = map { "vf.text_value REGEXP '(^|[^0-9])$_.docid'" } @docids;
+     my @regexp_query = map { "vf.text_value REGEXP '(^|[^0-9])$_\\\.docid'" } @docids;
      
      my $append = "( " . (join " OR ", @regexp_query) . ")";
      my $sql = <<END;
@@ -174,31 +183,34 @@ END
 
 			      
 sub find_referrers {
-     my ($this, $docs) = @_;
+     my ($this, $docids) = @_;
      
-     my @docids = map {$_->{docid}} grep { $_->{docid} } @$docs;
+     my $obvius = $this->{obvius};
+     $docids = $this->check_vfields_for_docids($docids);
      
-     my $docids = $this->check_vfields_for_docids(@docids);
-     
-     my @urls;
+     my @uris;
 
      for my $docid (@$docids) {
 	  my $doc = $obvius->get_doc_by_id($docid);
-	  push @urls, $obvius->get_doc_uri($doc) if ($doc);
+	  push @uris, $obvius->get_doc_uri($doc) if ($doc);
      }
-     my @res = map { {command => 'clear_url', url => $_} } @urls;
+     my @res = map { {command => 'clear_uri', uri => $_} } @uris;
      
      return \@res;
 }
      
-sub is_relevant_for_related_cache {
+sub is_relevant_for_leftmenu_cache {
      my ($this, $docid, $vdoc) = @_; 
      my @relevant_fields = qw(title short_title seq);
 
      my $obvius = ref $this eq 'Obvius' ? $this : $this->{obvius};
-
+     
      my $doc = $obvius->get_doc_by_id($docid);
+     return 0 if (!$doc);
+
      my $old_vdoc = $obvius->get_public_version($doc);
+     return 0 if (!$old_vdoc);
+     
      $obvius->get_version_fields($old_vdoc, \@relevant_fields);
      $obvius->get_version_fields($vdoc, \@relevant_fields);
 
@@ -209,39 +221,135 @@ sub is_relevant_for_related_cache {
      return 0;
 }
 
-sub find_dirty {
-     my ($this, $uris} = @_;
+
+sub perform_command_clear_doctype {
+     my ($this, $doctype_name) = @_;
+     my $obvius = $this->{obvius};
      
-     my $referrers = $this->find_referrers($uris);
+     my $doctype = $obvius->get_doctype_by_name($doctype_name);
+
+     my $query = <<END;
+SELECT DISTINCT(docid) FROM 
+    documents d INNER JOIN versions v ON (v.docid = d.id)
+WHERE
+    v.public = 1 AND (d.type = ? OR v.type = ?);
+END
+     my $docids = $this->execute_query($query, $doctype->Id, $doctype->Id);
      
-     my $related = $this->find_related($uris);
+     my @commands;
      
-     my @commands = (@referrers, @$related);
+     for (@$docids) {
+	  my $doc = $obvius->get_doc_by_id($_->{docid});
+	  my $path = $obvius->get_doc_uri($doc) if ($doc); 
+	  push @commands, {command => 'clear_uri', uri => $path} if($path);
+     }
+     
+     return \@commands;
+}
+	 
+sub special_actions {
+     my ($this, $docids) = @_;
+     my $obvius = $this->{obvius};
+
+     my @commands;
+     my %special_op_per_doctype = ( 
+				   Nyhed => {command => 'clear_doctype',  args => ['Nyhedsliste'] },
+				   CalendarEvent => {command => 'clear_doctype', args => ['Arragementsliste']}
+				  );
+     
+     for (@$docids) {
+     
+	  my $doc = $obvius->get_doc_by_id($_);
+	  my $doctype = $obvius->get_doctype_by_id($doc->Type);
+	  
+	  if (my $cmd = $special_op_per_doctype{$doctype->{NAME}}) {
+	       my $func = "perform_command_" . $cmd->{command};
+	       my $cmds = $this->$func(@{$cmd->{args}});
+	       push @commands, @$cmds;
+	  }
+     }
+
      return \@commands;
 }
 
+sub find_dirty {
+     my ($this, $cache_objects) = @_;
 
-sub find_and_flush {
-     my ($this, $uris) = @_;
+     my $vals = $cache_objects->request_values('uri', 'docid', 'clear_leftmenu');
+     my @uris		= grep { $_ } map { $_->{uri}   } @$vals;
+     my @docids		= grep { $_ } map { $_->{docid} } @$vals;
+     my @leftmenu_uris	= map { $_->{uri} } grep {  $_->{uri} and $_->{clear_leftmenu}} @$vals;
      
-     my $commands = $this->find_dirty($uris);
+     my $referrers = $this->find_referrers(\@docids);
+     my @related = map { $this->find_related($_) } @leftmenu_uris;
+     my $special_actions = $this->special_actions(\@docids);
      
-     $this->flush($commands);
+     my @commands = grep { $_ } 
+       (@$referrers, 
+	@related, 
+	@$special_actions,
+	map { { command => 'clear_uri', uri => $_}} @uris);
+     
+     return uniquify_commands(\@commands);
 }
 
-#Clear the whole site's cache.
-#Unless we are at the root.
+sub uniquify_commands {
+     my $commands = shift;
+     
+     my @result;
+     
+     OUTER: for my $cmd1 (@$commands) {
+	  for my $cmd2 (@result) {
+	       next OUTER if (commands_equal_p($cmd1, $cmd2));
+	  }
+	  push @result, $cmd1;
+     }
+
+     return \@result;
+}
+
+sub commands_equal_p {
+     my ($cmd1, $cmd2) = @_;
+     
+     for (keys %$cmd1, keys %$cmd2) {
+	  return 0 if ($cmd1->{$_} and $cmd2->{$_} and $cmd1->{$_} ne $cmd2->{$_});
+     }
+
+     return 1;
+}
+
+sub quick_flush {
+    my ($this, $cache_objects) = @_;;
+    
+    my $uris = $cache_objects->retrieve_values('uri', 'quick');
+    my %uris = map { $_->{uri} => 1} grep { $_->{uri} and $_->{quick} } @$uris;
+    
+    $this->flush_by_pattern(sub {return 1 if $uris{shift}; return 0; }) if(scalar keys %uris);
+}
+
+sub find_and_flush {
+     my ($this, $cache_objs) = @_;
+     
+     my $commands = $this->find_dirty($cache_objs);
+     
+     @$commands = grep {$_} @$commands;
+
+     $this->flush($commands) if scalar(@$commands);
+}
+
 sub find_related {
-     my ($this, $uris) = @_;
+     my ($this, $uri) = @_;
      
      my $obvius = $this->{obvius};
 
      my $hostmap = Obvius::Hostmap->new_with_obvius($obvius);
 
-     my $host_prefix = $hostmap->find_host_prefix($uris);
-     
-     return $host_prefix ? { command => 'clear_by_regexp', regexp => "^$host_prefix" } : 
-                           { command => 'clear_by_not_regexp', regexp => $hostmap->{regexp} };
+     my $host_prefix = $hostmap->find_host_prefix($uri);
+     if ($host_prefix) {
+	  return { command => 'clear_by_regexp', regexp => "^$host_prefix" } 
+     } else {
+	  return { command => 'clear_by_not_regexp', regexp => $hostmap->{regexp} };
+     }
 }
 
 1;
