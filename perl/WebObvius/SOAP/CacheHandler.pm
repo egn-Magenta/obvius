d122c9b0bf67fc45b51e11428b8c0103ad658afd
troels <troels>
2008-03-02 16:04:13 +0000

    New soap-handler, and slightly shorter too.

---
+++ perl/WebObvius/SOAP/CacheHandler.pm
@@ -0,0 +1,30 @@
+package WebObvius::SOAP::CacheHandler;
+
+use strict;
+use warnings;
+
+use Obvius;
+use Obvius::Config; 
+use Data::Dumper;
+use WebObvius::Cache::ApacheCache;
+
+my $obvius_config;
+
+sub import {
+     my ($class, $config) = @_;
+     $obvius_config = Obvius::Config->new($config);
+}
+
+sub flush {
+     my ($this, $commands) = @_;
+     
+     my $obvius = Obvius->new($obvius_config); 
+     my $ac = WebObvius::Cache::ApacheCache->new($obvius);
+     $ac->flush($commands);
+
+     return 0;
+}
+     
+
+
+1;
