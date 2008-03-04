1fe9f234b713e13ed93f4525c3ef9b7318c5c1b2
troels <troels>
2008-03-04 07:54:14 +0000

    Added new Leftmenucache.

--- perl/WebObvius/Cache/Cache.pm
+++ perl/WebObvius/Cache/Cache.pm
@@ -2,21 +2,21 @@ package WebObvius::Cache::Cache;
 
 use strict;
 use warnings;
-use WebObvius::Cache::LeftmenuMasonCache;
+
 use WebObvius::Cache::ExternalCache;
 use WebObvius::Cache::Collection;
+use WebObvius::Cache::FileCache;
 
 our @ISA = qw( WebObvius::Cache::Collection );
 
 sub new {
      my ($class, $obvius) = @_;
      
-     my $leftmenu_cache = WebObvius::Cache::LeftmenuMasonCache->new($obvius->{OBVIUS_CONFIG}{SITEBASE}, 
-							    '/portal/subdocs', 'get_subs');
+     my $leftmenu_cache = WebObvius::Cache::FileCache->new($obvius);
 
      my $external_cache = WebObvius::Cache::ExternalCache->new($obvius);
      
-     return $class->SUPER::new($external_cache, $leftmenu_cache);
+     return $class->SUPER::new($leftmenu_cache, $external_cache);
 
 }
 
