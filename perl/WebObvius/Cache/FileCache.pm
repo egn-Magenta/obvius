02a6b4f711944c0207afae597fc61472fb662b79
troels <troels>
2008-03-04 11:51:46 +0000

    *** empty log message ***

--- perl/WebObvius/Cache/FileCache.pm
+++ perl/WebObvius/Cache/FileCache.pm
@@ -7,41 +7,19 @@ use Data::Dumper;
 use Cache::FileCache;
 
 sub new {
-     my ($class, $obvius, %opts) = @_;
+     my ($class, $obvius, $namespace) = @_;
 
-     my $this = {obvius => $obvius, %opts};
+     my $cache_dir = $obvius->{OBVIUS_CONFIG}{FILECACHE_DIRECTORY};
+     my $this = {obvius => $obvius, namespace => $namespace, cache_root => $cache_dir};
      return bless $this, $class;
 }
 
-sub find_dirty {
-     my ($this, $cache_objects) = @_;
-     my @dirty;
-
-     my $obvius = $this->{obvius};
-     
-     $obvius->connect if (!$obvius->{DB});
-     my $values = $cache_objects->request_values('docid', 'clear_leftmenu', 'clear_admin_leftmenu');
-     my @docids = map { $_->{docid} } 
-       grep { $_->{clear_admin_leftmenu} || $_->{clear_leftmenu}} @$values;
-     
-     for (@docids) {
-	  push @dirty, $_;
-	  my $doc = $obvius->get_doc_by_id($_);
-	  next if (!$doc);
-	  
-	  push @dirty, $doc->Parent if ($doc->Parent);
-
-     }
-     
-     return \@dirty;
-}
- 
 sub flush {
      my ($this, $dirty) = @_;
      $dirty = [ $dirty] if (!ref $dirty);
      
-     my $cache = Cache::FileCache->new({cache_root => '/var/www/www.ku.dk/var/file_cache',
-					namespace => 'subdocs'});
+     my $cache = Cache::FileCache->new({cache_root => $this->{cache_root},
+					namespace => $this->{namespace}});
 
      $cache->remove($_) for (@$dirty);
 }
