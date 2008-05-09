57e30a64c7b74f923f25a2f6c916ca592c5169a8
troels <troels>
2008-05-09 10:52:23 +0000

    New ExternalApacheCache-api.

---
+++ perl/WebObvius/Cache/ExternalApacheCache.pm
@@ -0,0 +1,32 @@
+package WebObvius::Cache::ExternalApacheCache;
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+use WebObvius::Cache::SOAPHelper;
+our @ISA = qw( WebObvius::Cache::ApacheCache );
+
+sub new {
+     my ($class,  @args) = @_;
+     
+     my $new = $class->SUPER::new(@args);
+     return $new;
+}
+
+
+sub find_and_flush {
+     my ($this, $cache_objs) = @_;
+     
+     my $commands = $this->find_dirty($cache_objs);
+     
+     @$commands = grep {$_} @$commands;
+     if (scalar @$commands) {
+	  $this->flush($commands);
+	  my $command = {cache => 'WebObvius::Cache::ApacheCache', commands => $commands}; 
+	  WebObvius::Cache::SOAPHelper->send_command($this, $command);
+     }
+}
+
+
+1;
