dda1aa7991a222b9b643693ad325b89103c5efb5
Troels x <troels@tesla.adm.ku.dk>
2009-02-02 11:41:02 +0100

    Create link script, for setting up new obvius instance.

---
+++ bin/create_links.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+ln -s /var/www/obvius/docs/grafik/admin /var/www/obvius/docs/grafik/admin/admin
+ln -s /var/www/obvius/docs/grafik/admin/navigator /var/www/obvius/docs/grafik/admin/navigator/navigator
+ln -s /var/www/tinymce/from_obvius/tiny_mce /var/www/obvius/docs/js/tiny_mce
+ln -s /var/www/tinymce/from_obvius/tiny_mce /var/www/obvius/docs/js/tiny_mce_old/tiny_mce
+ln -s /var/www/obvius/docs/pics/icons /var/www/obvius/docs/pics/icons/icons
+ln -s /var/www/obvius/docs/scripts /var/www/obvius/docs/scripts/scripts
+ln -s /var/www/tinymce/from_obvius/tiny_mce /var/www/obvius/docs/scripts/tiny_mce
+ln -s /var/www/tinymce/tinymce_editor_component /var/www/obvius/mason/common/shared/editengine2/type/editors/tinymce
