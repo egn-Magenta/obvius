ca6900833646741ee0e750ba3aa19847e6228a76
Dennis Isaksen <dennis@magenta-aps.dk>
2014-02-27 11:58:42 +0100

    Actually also changed the DB structure in the update script.

--- sql/updates/20140207_statistics_on_individual_pages.sql
+++ sql/updates/20140207_statistics_on_individual_pages.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `monthly_path_statisics` (
         `id` int(11) NOT NULL AUTO_INCREMENT,
         `subsite` int(11) DEFAULT NULL,
-        `month` tinyint(4) NOT NULL,
+        `yearmonth` tinyint(4) NOT NULL,
         `uri` varchar(512) DEFAULT NULL,
         `visit_count` int(11) DEFAULT NULL,
         PRIMARY KEY (`id`),
