3ee030e0743942b545d3f64145a62a333f119754
Jørgen Ulrik B. Krag <jubk@magenta-aps.dk>
2016-06-30 16:30:21 +0200

    - Add global sitecommands method to mason components that can be   used to access a site-specific perl module with methods that   have access to global variables. - Import Data::Dumper in all mason components!

--- perl/WebObvius/MasonCommands.pm
+++ perl/WebObvius/MasonCommands.pm
@@ -3,10 +3,44 @@ package WebObvius::MasonCommands;
 use strict;
 use warnings;
 
+use vars qw($m $doc $mcms $prefix $uri $vdoc $obvius $doctype $r);
+
+use Data::Dumper;
+
 use Obvius::Translations ();
 
 # Global translation methods
 *__ = \&Obvius::Translations::gettext;
 *gettext = \&Obvius::Translations::gettext;
+
+# Return the package name used for site-specific commands.
+sub sitecommands {
+    if($r) {
+        my $classname = $r->notes('site_packagename');
+        if(!defined($classname)) {
+            $classname = 'WebObvius::MasonCommands';
+            if($obvius) {
+                $classname = $obvius->config->param('site_packagename') || '';
+            }
+            if($classname) {
+                $classname->initialize(
+                    m => \$HTML::Mason::Commands::m,
+                    doc => \$doc,
+                    mcms => \$mcms,
+                    prefix => \$prefix,
+                    uri => \$uri,
+                    vdoc => \$vdoc,
+                    obvius => \$obvius,
+                    doctype => \$doctype,
+                    r => \$r,
+                );
+            }
+            $r->notes('site_packagename' => $classname);
+        }
+        return $classname;
+    }
+
+    return 'WebObvius::MasonCommands';
+}
 
 1;
