3ec1c6e6653218900c8ccee96ccd22ae33a14faa
Tereza Lansing <tl@magenta.dk>
2019-05-20 19:33:07 +0000

    character countdown

--- docs/js/formdata/formdata_new_admin.js
+++ docs/js/formdata/formdata_new_admin.js
@@ -1465,3 +1465,12 @@ function OpenWin(url, w, h) {
                   +'width='+w+',height='+h);
   window.open (url + '', '', features);
 }
+
+function charcountupdate(str) {
+    console.log('charcountupadate');
+    var lng = str.length;
+    var total = 255;
+    var remaining = total - lng;
+
+    document.getElementById("charcount").innerHTML = ' ' + remaining;
+}
