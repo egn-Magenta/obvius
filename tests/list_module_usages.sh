cd1a57b0328a6006fa8db491017635fd0dd7f151
Emil Thorenfeldt <emt@magenta-aps.dk>
2020-04-23 15:58:05 +0200

    Add FakeRequest and Module::Install to ignore list

--- tests/list_module_usages.sh
+++ tests/list_module_usages.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 FOLDERS_TO_CHECK="$1"
 # Apache2::SizeLimit & compat require live mod_perl environments
+# Apache2::FakeRequest causes weird errors due to dynamic loading in mod perl
 # Apache::File & Text::Format are still included in mason/admin/action/send
 # Text::Aspell & XML::DOM live in most likely deprecated cgi spellcheck script
 # WebObvius::Cache::Flushing is referenced in old SOAP functionality
 # Formengine dynamically loads classes
 # WebObvius::Site loads Apache2::SizeLimit
 # use mysql; comes from sql script
-EXCLUDE_MODULES="Apache2::SizeLimit|Apache2::compat|Apache::File|Text::Format|Text::Aspell|XML::DOM|WebObvius::Cache::Flushing|WebObvius::FormEngine::Fields::MultipleBase|WebObvius::Site|use mysql;|use strict;|use warnings;|use utf8;"
+EXCLUDE_MODULES="Apache2::FakeRequest|Apache2::SizeLimit|Apache2::compat|Apache::File|Module::Install|Text::Format|Text::Aspell|XML::DOM|WebObvius::Cache::Flushing|WebObvius::FormEngine::Fields::MultipleBase|WebObvius::Site|use mysql;|use strict;|use warnings;|use utf8;"
 
 if [ -z "$FOLDERS_TO_CHECK" ]; then
     echo "Please specify target folders"
